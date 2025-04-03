d6b15e5ef207e240542a734dc325631fdd80eef3
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-03 20:33:57 +0100

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,14 +4,16 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
+import java.sql.SQLOutput;
+
 
 public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
